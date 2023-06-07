2807f37e3a5026200c523ae43dd44545fe80b125
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-06-07 13:10:19 -0400

    flash notice handling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,28 @@
 class ArticlesController < ApplicationController
 
     def show
-        @article = Article.find(params[:article])
+        @article = Article.find(params[:id])
+        
     end
 
     def index 
         @articles = Article.all
+    end
+
+    def new
+
+        @article = Article.new
+
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description, :content))
+       if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to @article
+        else
+            render 'new' 
+       end
     end
 end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+
+    validates :title, presence: true
+
+    validates :description, presence: true
+    
+    validates :content, presence: true
+
 end
